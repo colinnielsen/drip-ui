fc08d429685d751dff886d75a20cb32cd94e6ae8
Colin Nielsen <33375223+colinnielsen@users.noreply.github.com>
2024-07-16 18:02:04 -0600

    feat: move base components

--- src/components/base/Divider.tsx
+++
@@ -1,5 +0,0 @@
-import { cn } from '@/lib/utils';
-
-export const Divider = ({ className }: { className?: string }) => {
-  return <div className={cn('border-b border-light-gray', className)} />;
-};

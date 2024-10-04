3742fabcea68594deff6e3eb10553af08c48d834
Colin Nielsen <33375223+colinnielsen@users.noreply.github.com>
2024-10-04 16:59:24 -0600

    feat: add params

--- src/pages/api/slice/get-store-products.ts
+++ src/pages/api/slice/get-store-products.ts
@@ -8,6 +8,7 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     return;
   }
 
+  console.log('params', { ...req.body, dynamicPricing: true });
   await sliceKit
     .getStoreProducts({ ...req.body, dynamicPricing: true })
     .then(r => res.status(200).json(r))

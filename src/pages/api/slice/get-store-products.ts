76ba2d53f62e4e3ed5a0f990eadf71428097bba7
Colin Nielsen <33375223+colinnielsen@users.noreply.github.com>
2024-10-07 09:52:02 -0600

    feat: push up error message instead

--- src/pages/api/slice/get-store-products.ts
+++ src/pages/api/slice/get-store-products.ts
@@ -22,10 +22,10 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   })
     .then(r => res.status(200).json(r))
     .catch(error => {
-      console.log('error', error);
+      console.log('error', error.message);
       return res
         .status(500)
-        .json({ message: 'Internal Server Error', error: error });
+        .json({ message: 'Internal Server Error', error: error.message });
     });
 };
 

03dfaa5ccc7d602d092c04745d2496db538c6b0a
Colin Nielsen <33375223+colinnielsen@users.noreply.github.com>
2025-01-27 15:21:02 -0700

    remove client only on the manage page

--- src/pages/manage.tsx
+++ src/pages/manage.tsx
@@ -1,24 +1,21 @@
-import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
-import { PageWrapper } from '@/components/ui/page-wrapper';
 import { SquareConnectionManager } from '@/components/manage-page/square';
-import { ClientOnly } from '@/components/ui/client-only';
+import { PageWrapper } from '@/components/ui/page-wrapper';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 
 export default function ManagePage() {
   return (
     <PageWrapper>
-      <ClientOnly>
-        <div className="flex flex-col items-center justify-center py-20 gap-6 px-10">
-          <Tabs className="w-full" defaultValue="square">
-            <TabsList>
-              <TabsTrigger value="square">Square</TabsTrigger>
-              <TabsTrigger value="slice">Slice</TabsTrigger>
-            </TabsList>
-            <TabsContent value="square">
-              <SquareConnectionManager />
-            </TabsContent>
-          </Tabs>
-        </div>
-      </ClientOnly>
+      <div className="flex flex-col items-center justify-center py-20 gap-6 px-10">
+        <Tabs className="w-full" defaultValue="square">
+          <TabsList>
+            <TabsTrigger value="square">Square</TabsTrigger>
+            <TabsTrigger value="slice">Slice</TabsTrigger>
+          </TabsList>
+          <TabsContent value="square">
+            <SquareConnectionManager />
+          </TabsContent>
+        </Tabs>
+      </div>
     </PageWrapper>
   );
 }

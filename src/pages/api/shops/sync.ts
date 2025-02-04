82e3763def62c16c0df5ed5d4e4051926ca6d1f2
Colin Nielsen <33375223+colinnielsen@users.noreply.github.com>
2025-02-03 17:05:09 -0700

    ft: revalidate thing

--- src/pages/api/shops/sync.ts
+++ src/pages/api/shops/sync.ts
@@ -45,8 +45,8 @@ export default ApiRoute(async function (
     await syncService
       .syncStore(externalId)
       .then(async shopId => {
-        await res.revalidate('/');
-        await res.revalidate(`/shops/${shopId}`);
+        await revalidatePathIfExists(res, '/');
+        await revalidatePathIfExists(res, `/shops/${shopId}`);
         return res.status(200).json({ shopId });
       })
       .catch(err => {

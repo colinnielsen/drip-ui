37772fa2268622f23961a7aebf92e7c0dfb9cdb3
Colin Nielsen <33375223+colinnielsen@users.noreply.github.com>
2025-06-21 15:13:35 -0700

    feat: update CB button

--- src/components/cart/onramp/onramp.tsx
+++ src/components/cart/onramp/onramp.tsx
@@ -22,22 +22,20 @@ export const GetUSDCButton = () => {
     <div className="flex flex-col items-center justify-center h-full">
       <button
         onClick={() => window.open(quote.url)}
-        className="bg-black text-white px-4 rounded-[100px] flex items-center gap-[3px] w-full justify-center py-[11px] h-[50px] font-['-apple-system',BlinkMacSystemFont,sans-serif]"
+        className="bg-[#0052FF] text-white px-4 rounded-[100px] flex items-center gap-2 w-full justify-center py-[11px] h-[50px] font-semibold text-lg hover:bg-[#0048e5] transition-colors"
       >
         <svg
+          viewBox="0 0 146 146"
           xmlns="http://www.w3.org/2000/svg"
-          xmlSpace="preserve"
-          width="24"
-          height="24"
-          viewBox="0 0 1000 1000"
-          className="h-[18px]"
+          className="h-6 w-6"
         >
+          <circle cx="73" cy="73" r="73" fill="#0052FF" />
           <path
+            d="M73.323 123.729C101.617 123.729 124.553 100.832 124.553 72.5875C124.553 44.343 101.617 21.4463 73.323 21.4463C46.4795 21.4463 24.4581 42.0558 22.271 68.2887H89.9859V76.8864H22.271C24.4581 103.119 46.4795 123.729 73.323 123.729Z"
             fill="white"
-            d="M788.1 340.9c-5.8 4.5-108.2 62.2-108.2 190.5 0 148.4 130.3 200.9 134.2 202.2-.6 3.2-20.7 71.9-68.7 141.9-42.8 61.6-87.5 123.1-155.5 123.1s-85.5-39.5-164-39.5c-76.5 0-103.7 40.8-165.9 40.8s-105.6-57-155.5-127C46.7 790.7 0 663 0 541.8c0-194.4 126.4-297.5 250.8-297.5 66.1 0 121.2 43.4 162.7 43.4 39.5 0 101.1-46 176.3-46 28.5 0 130.9 2.6 198.3 99.2zm-234-181.5c31.1-36.9 53.1-88.1 53.1-139.3 0-7.1-.6-14.3-1.9-20.1-50.6 1.9-110.8 33.7-147.1 75.8-28.5 32.4-55.1 83.6-55.1 135.5 0 7.8 1.3 15.6 1.9 18.1 3.2.6 8.4 1.3 13.6 1.3 45.4 0 102.5-30.4 135.5-71.3z"
           />
         </svg>
-        <p className="font-semibold text-[23px]">Pay</p>
+        <span>Top up with Coinbase</span>
       </button>
       {/* <CTAButton>Buy with Apple Pay</CTAButton> */}
     </div>
